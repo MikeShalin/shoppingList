d311a2816329fc91d5b73affa3add4680ac14edb
mike <mixailshalin@yandex.ru>
2018-03-25 10:39:28 +0400

    devel db connect

--- source/js/back/connect.js
+++ source/js/back/connect.js
@@ -4,8 +4,8 @@
 const mysql = require('mysql');
 module.exports = mysql.createConnection({
     host     : '127.0.0.1',
-    user     : 'root',
-    password : 'root',
+    user     : 'root', //mikeshalin_shop
+    password : 'root', //1Ayei&0d
     database : 'shoppingList'
 });
 
